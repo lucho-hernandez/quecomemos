dcb7923b98837f84f9e4ed734cb9ce8e42a20e4b
lucho-hernandez <40920247+lucho-hernandez@users.noreply.github.com>
2018-10-03 18:13:13 -0300

    First commit

---
+++ food.js
@@ -0,0 +1,29 @@
+(function() {
+  var quotes;
+  fetch("http://gsx2json.com/api?id=1FItBw1ljvC1K09yK-Tmu5_hV0g7hI_XKCBN_ve5m3Sg&sheet=1&columns=false")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    populateData(myJson.rows)
+    document.getElementById("loader").style.display = "none";;
+  });
+  
+var populateData = function(data) {
+
+  var randomQuoteIndex = Math.floor(Math.random() * data.length);
+
+  var randomQuote = data[randomQuoteIndex];
+
+  var foodElement = document.getElementById("food");
+  var imageElement = document.getElementById("image-food");
+  var buttonElement = document.getElementById("search-button")
+  var buttonOrder = document.getElementById("order-button")
+
+  foodElement.innerText = randomQuote.text;
+  imageElement.src = randomQuote.img;
+  buttonElement.href = "https://www.google.com/search?q=Receta+de+"+randomQuote.text;
+  buttonOrder.href = "https://www.pedidosya.com.ar/restaurantes/cordoba?&v="+randomQuote.text;
+  }
+  
+})();
